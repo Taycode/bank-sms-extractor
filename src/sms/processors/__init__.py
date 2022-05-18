27b4e1602d92ef801851fcbd4c25e808d8f73862
Abdulmateen Tairu <tay2druh@gmail.com>
2022-05-18 20:47:36 +0100

    Mapped UBA and Keystone bank processors

--- src/sms/processors/__init__.py
+++ src/sms/processors/__init__.py
@@ -1,8 +1,12 @@
 from src.sms.processors.first_bank import first_bank_processor
 from src.sms.processors.access_bank import access_bank_processor
 from src.sms.processors.gtbank import gtbank_processor
+from src.sms.processors.keystone_bank import keystone_bank_processor
+from src.sms.processors.uba import uba_processor
 bank_processors = {
     "FirstBank": first_bank_processor,
     "AccessBank": access_bank_processor,
     "GTBank": gtbank_processor,
+    "KEYSTONE": keystone_bank_processor,
+    "UBA": uba_processor,
 }
