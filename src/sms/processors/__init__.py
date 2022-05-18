4811086c8eed2be30212d4e1b0b43153f93d5ea3
Abdulmateen Tairu <tay2druh@gmail.com>
2022-05-18 20:27:42 +0100

    Mapped GTbank processor

--- src/sms/processors/__init__.py
+++ src/sms/processors/__init__.py
@@ -1,6 +1,8 @@
 from src.sms.processors.first_bank import first_bank_processor
 from src.sms.processors.access_bank import access_bank_processor
+from src.sms.processors.gtbank import gtbank_processor
 bank_processors = {
     "FirstBank": first_bank_processor,
     "AccessBank": access_bank_processor,
+    "GTBank": gtbank_processor,
 }
