bf29645022a90522c1b1fe32badd2998f6609ea3
Abdulmateen Tairu <tay2druh@gmail.com>
2022-05-18 20:16:47 +0100

    Mapped Processors to senders

---
+++ src/sms/processors/__init__.py
@@ -0,0 +1,6 @@
+from src.sms.processors.first_bank import first_bank_processor
+from src.sms.processors.access_bank import access_bank_processor
+bank_processors = {
+    "FirstBank": first_bank_processor,
+    "AccessBank": access_bank_processor,
+}
--- src/sms/sms_controller.py
+++ src/sms/sms_controller.py
@@ -2,6 +2,7 @@ from flask_restx import Namespace, Resource
 from flask import request
 from src.sms.sms_dto import process_sms_dto
 from src.sms.processors.first_bank import first_bank_processor
+from src.sms.processors import bank_processors
 
 
 sms_namespace = Namespace('Sms', 'SMS Module')
@@ -15,5 +16,9 @@ class ProcessSms(Resource):
     def post(self):
         payload = request.json
         message = payload['message']
-        data = first_bank_processor(message)
+        sender = payload['sender']
+        processor = bank_processors.get(sender)
+        if not processor:
+            return {'message': 'Not a bank'}, 400
+        data = processor(message)
         return data
