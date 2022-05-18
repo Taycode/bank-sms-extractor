d12fabd79a8427a84471e107da7b0344c88b81e6
KehindeBello <adedotunomomeji@gmail.com>
2022-05-18 20:26:26 +0100

    Keystone Bank Processor

---
+++ src/sms/processors/keystone_bank.py
@@ -0,0 +1,43 @@
+import re 
+
+def keystone_bank_processor(message: str):
+    """Processess Keystone Bank sms"""
+
+    #Fetch Transaction Type
+    transaction_type = re.findall(r'Debit|Credit', message)
+    transaction_type = transaction_type[0]
+
+    #Fetch account number
+    account_number = re.findall(r'Acct:\w+[*]+\d+', message)
+    account_number = account_number[0]
+    account_number = account_number[len('Acc:'):]
+
+    #Fetch Description
+    description = re.findall(r'Desc:.*?(?:(?!\\n).)*', message)
+    description = description[0]
+    description = description[len('Desc:'):]
+
+    #Fetch Amount
+    amount = re.findall(r'Amt:N.*?(?:(?!\\n).)*', message)
+    amount = amount[0]
+    amount = amount[len('Amt:N'):]
+
+    #Fetch Date
+    date = re.findall(r'Date:.*?(?:(?!\\n).)*', message)
+    date = date[0]
+    date = date[len('Date:'):]
+
+    #Fetch Balance
+    balance = re.findall(r'Bal:NGN*?(?:(?!\\n).)*', message)
+    balance = balance[0]
+    balance = balance[len('Bal:NGN'):]
+
+
+    return {
+        'description': description,
+        'transaction_type': transaction_type,
+        'account_number': account_number,
+        'amount': amount,
+        'balance': balance,
+        'date': date
+    }
