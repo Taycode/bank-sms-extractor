72ecde68ac973ddc4bd4e94da3b4b3efaa8e94a8
KehindeBello <adedotunomomeji@gmail.com>
2022-05-19 20:56:25 +0100

    Zenith Bank Processor

---
+++ src/sms/processors/zenith_bank.py
@@ -0,0 +1,42 @@
+import re
+
+
+def zenith_bank_processor(message: str):
+    """Processes Zenith Bank SMS"""
+
+    # Fetch Transaction Type
+    transaction_type = re.findall(r'(CR|DR) Amt', message)
+    transaction_type = transaction_type[0]
+
+    # Fetch account number
+    account_number = re.findall(r'Acct:\w+[.*]+\d+\w'')', message)
+    account_number = account_number[0]
+    account_number = account_number[len('Acct:'):]
+
+    # Fetch Description
+    description = re.findall(r'(?<=\n)(.*?)(?=\n)', message)[1]
+
+    # Fetch Amount
+    amount = re.findall(r'Amt:[\d+,]+.\d+', message)
+    amount = amount[0]
+    amount = amount[len('Amt:'):]
+
+    # Fetch Date
+    date = re.findall(r'DT:\d{2}/\d{2}/\d{4}:\d+:\d+', message)
+    date = date[0]
+    date = date[len('DT:'):]
+
+    # Fetch Balance
+    balance = re.findall(r'Bal:[\d+,]+.\d+', message)
+    balance = balance[0]
+    balance = balance[len('Bal:'):]
+
+
+    return {
+        'description': description,
+        'transaction_type': transaction_type,
+        'account_number': account_number,
+        'amount': amount,
+        'balance': balance,
+        'date': date
+    }
